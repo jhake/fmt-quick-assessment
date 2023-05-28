b02f6f39604175c2fb4e5a0d0d557ba8438ec8df
Jhake <jhake.aquino123@gmail.com>
2023-05-29 03:39:04 +0800

    temp test if working

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,9 @@ export default function Home({
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Map flightId={flightId} />
-      <FlightSearch flightId={flightId} />
+      {/* <Map flightId={flightId} />
+      <FlightSearch flightId={flightId} /> */}
+      TEST
     </main>
   );
 }
